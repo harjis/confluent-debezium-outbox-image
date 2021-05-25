88eb9db7feff2ba7410c586d1f7b461c5ad09008
Joonas Harjukallio <harjukallio@gmail.com>
2021-05-25 09:37:26 +0300

    Add CI/CD pipeline Fix connector deprectated values (table./schema.whitelist)

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
-docker build -t d0rka/confluent-debezium-outbox-image:latest \
+if [ -z "$VERSION" ]
+then
+      echo "\$VERSION is empty"
+      exit 1
+fi
+
+docker build \
+-t d0rka/confluent-debezium-outbox-image:latest \
+-t d0rka/confluent-debezium-outbox-image:$VERSION \
 -f ./kafka-connect/Dockerfile ./kafka-connect
+
 docker push d0rka/confluent-debezium-outbox-image:latest
+docker push d0rka/confluent-debezium-outbox-image:$VERSION
